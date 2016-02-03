f3c2465867b5aaabcead28482e408b57c27e313b
pchinjr <pchinjr@gmail.com>
2016-02-03 00:22:06 -0500

    added more ui, you can buy

--- main.js
+++ main.js
@@ -16,40 +16,56 @@ var CatPrices = {
 
 Player.prototype.buy = function(type) {
   if(this.bank <= 0){
+    $('#warning').html("You don't have enough money to buy this catte :(");
     console.log("You don't have enough money to buy this catte :(");
+    render();
     return;
   }
   if(this.stash.length >= 10) {
+    $('#warning').html("You can't hold any more cattes :(");
     console.log("You can't hold any more cattes :(");
+    render();
     return;
   }
   this.bank -= CatPrices[type];
   this.stash.push(type);
   console.log(this.name + ' buys a ' + type + ' catte.');
+  render();
 };
 
 Player.prototype.sell = function(type) {
   if(this.stash.length <=0) {
+    $('#warning').html('No more cattes to sell');
     console.log('No more cattes to sell');
+    render();
     return;
   }
   var index = $.inArray(type, this.stash);
   if(index < 0) {
+    $('#warning').html(type + ' cat not found in your stash');
     console.log(type + ' cat not found in your stash');
+    render();
     return;
   }
   this.stash.splice(index, 1);
   this.bank += CatPrices[type];
-  console.log('You have sold 1 ' + type + ' catte.')
+  console.log('You have sold 1 ' + type + ' catte.');
+  render();
 };
 
 Player.prototype.checkStash = function() {
+  var listStash = "";
+  for(var i=0; i< this.stash.length; i++) {
+    listStash += '<li>' + this.stash[i].toUpperCase() + '</li>';
+  }
+  $('#stash').html(listStash);
   console.log('You have ' + this.stash + ' in your stash');
 };
 
 Player.prototype.checkBank = function() {
   if(this.bank === 0) {
-    $('#bank').html('You have run out of money');
+    $('#warning').html('You have run out of money');
+    $('#bank').html(this.bank);
     console.log("You have run out of money :(");
     return;
   }
@@ -58,15 +74,23 @@ Player.prototype.checkBank = function() {
 };
 
 Player.prototype.checkPrices = function() {
+  var listPrices = ""
+  for (var i in CatPrices) {
+    listPrices += '<li>' + i.toUpperCase() + ': $' + CatPrices[i] + ".00" + '</li>';
+  }
+  $("#prices").html(listPrices);
   console.log('Local Prices are:' + '\n' + 'orange - ' + CatPrices.orange + ' / black - ' + CatPrices.black + ' / silver - ' + CatPrices.silver + ' / white - ' + CatPrices.white);
 };
 
 Player.prototype.jet= function(location) {
   this.location = location;
   days -= 1;
+  $('#location').html(this.location);
   console.log('You jet to ' + this.location);
+  $('#days').html(days);
   console.log('There are ' + days + ' days left' + '\r\n' + ' ');
   updatePrice();
+  render();
 };
 
 var updatePrice = function() {
@@ -84,16 +108,12 @@ var render = function() {
   p1.checkPrices();
 };
 
-var p1 = new Player('Paul', 100, [], 'Norfolk');
+var p1 = new Player('Paul', 100, [], 'NFK');
+
+
+$('#buy').on('click', function() {
+  var type = $('#type').val().toLowerCase();
+  p1.buy(type);
+});
 
 render();
-p1.buy('white');
-p1.jet('L.A.');
-
-render();
-p1.buy('orange');
-p1.jet('D.C.');
-
-render();
-p1.buy('black');
-p1.jet('ATL');
